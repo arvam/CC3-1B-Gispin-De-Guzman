bb7cdf34f9ca0ca2393dbbe9376b262fa977f24b
Gispin, De Guzman <46735839+arvam@users.noreply.github.com>
2019-01-16 15:21:36 +0800

    Add files via upload
    CC3-1B-Act 2

---
+++ Dog.java
@@ -0,0 +1,52 @@
+package classesandobjects;
+
+
+public class Dog
+{
+   
+    String name;
+    int age;
+    String breed;
+    char gender;
+   
+    public void setname(String dogname)
+    {
+        name = dogname;
+    }
+   
+    public String getname()
+    {
+        return name;       
+    }
+   
+    public void setage(int dogage)
+    {
+        age = dogage;
+    }
+   
+    public int getage()
+    {
+        return age;
+    }
+    public void setbreed(String dogbreed)
+    {
+        breed = dogbreed;
+    }
+   
+    public String getbreed()
+    {
+        return breed;
+    }
+    public void setgender(char doggender)
+    {
+        gender = doggender;
+    }
+   
+    public char getgender()
+    {
+        return gender;
+    }
+ 
+   
+}
+
